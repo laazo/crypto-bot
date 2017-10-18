32a07a56931232a8d9ccbadd7c10fc1e585c1845
Azola.Ndamase <azola@synthesis.co.za>
2017-10-18 13:30:24 +0200

    Add ethereum bot

--- cryptoBot.js
+++ cryptoBot.js
@@ -10,20 +10,48 @@ var bot = controller.spawn({
 }).startRTM();
 
 const BTCZAR_URL = 'https://api.mybitx.com/api/1/ticker?pair=XBTZAR';
-var previousPrice = 0;
+const ETHZAR_URL = 'https://api.coinmarketcap.com/v1/ticker/?convert=ZAR&limit=2';
+const options = ['ZAR', 'Bitcoin currently trading at R', 'Bitcoin price: R', 'R'];
 
+var opt;
+var previousBtPrice = 0;
+var previousEtPrice = 0;
+
+// Bitcoin listener
 controller.hears(['hey', 'hi', 'hello'], 'direct_mention, direct_message, mention', function(bot, message) {
-	var options = ['ZAR', 'Bitcoin currently trading at R', 'Bitcoin price: R', 'R'];
-	var opt = options[Math.floor((Math.random() * 3) + 1)];
+	opt = options[Math.floor((Math.random() * 3) + 1)];
 	
 	https.get(BTCZAR_URL, (response) => {
 		let bitCoinPrice = '';
 		let emoji = '';
 		response.on('data', (data) => {
 			bitCoinPrice = JSON.parse(data.toString()).bid;
-			emoji = previousPrice < parseFloat(bitCoinPrice) ? ':arrow_up:' : ':arrow_down:';
+			emoji = previousBtPrice < parseFloat(bitCoinPrice) ? ':arrow_up:' : ':arrow_down:';
 			bot.reply(message, opt + bitCoinPrice + ' ' + emoji);
-			previousPrice = parseFloat(bitCoinPrice);
+			previousBtPrice = parseFloat(bitCoinPrice);
+		});
+	}).on('error', (err) => { 
+		bot.reply(message, 'Oops! Can you try that again?');
+	});
+});
+
+// ethereum listener
+controller.hears(['eth', 'ethereum'], 'direct_mention, direct_message, mention', function(bot, message) {
+	opt = options[Math.floor((Math.random() * 3) + 1)];
+	
+	https.get(ETHZAR_URL, (response) => {
+		let ethereumPrice = '';
+		let emoji = '';
+		let chunks = [];
+		response.on('data', (data) => {
+			chunks.push(data);
+		}).on('end', () => { 
+			let res = Buffer.concat(chunks);
+			let apiResponse = JSON.parse(res);
+			ethereumPrice = parseFloat(apiResponse[1].price_zar);
+			emoji = previousEtPrice < ethereumPrice ? ':arrow_up:' : ':arrow_down:';
+			bot.reply(message, opt + ethereumPrice.toFixed(2) + ' ' + emoji);
+			previousEtPrice = ethereumPrice;
 		});
 	}).on('error', (err) => { 
 		bot.reply(message, 'Oops! Can you try that again?');
